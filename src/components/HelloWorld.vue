7c1f143fb018a6ee65974364b81c90d5c44ac33a
StefanVrecic <vstefan@hotmail.co.uk>
2021-07-31 00:41:15 +1000

    remove env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Stefan test {{process.env.TEST}} app
+          Stefan test app
         </h1>
 
         <p class="subheading font-weight-regular">
